6fe88b204ce91ee2dc12b255d104a198cb7cf8e0
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-17 09:02:12 -0800

    create products and reviews table in db

---
+++ db/migrate/20171117165503_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.column :name, :string
+      t.column :cost, :string
+      t.column :country_of_origin, :string
+
+      t.timestamps
+    end
+  end
+end
