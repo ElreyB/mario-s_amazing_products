a0e45ba94f24437448026de53af8969fd59eb82e
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-17 11:21:51 -0800

    remove extra comma in reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Review.destroy_all
   products = Product.create!(name: Faker::Commerce.product_name, cost: Faker::Commerce.price, country_of_origin: Faker::Address.country, created_at: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today))
 
   5.times do
-    products.reviews.create!(author: Faker::RickAndMorty.character, content: Faker::Lorem.sentences(4).join(","), rating: Faker::Number.between(1, 5), created_at: Faker::Date.between(24.days.ago, Date.today))
+    products.reviews.create!(author: Faker::RickAndMorty.character, content: Faker::Lorem.sentences(4).join(" "), rating: Faker::Number.between(1, 5), created_at: Faker::Date.between(24.days.ago, Date.today))
   end
 end
 
