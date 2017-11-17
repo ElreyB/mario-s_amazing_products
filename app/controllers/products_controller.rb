ba865ece263ab2536cf4fef84806eb6bfa08d2de
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-17 10:26:41 -0800

    create CRUD and views for Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,48 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Your product has been saved!"
+      redirect_to products_path
+    else
+      render new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
     @product =Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Your product has been update!"
+      redirect_to product_path(@product)
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
   end
 
 end
