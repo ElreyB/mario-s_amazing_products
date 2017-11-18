ce42bb04ca963823f2fc405aff69850bd49993d2
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-17 16:00:36 -0800

    WIP integration test

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+describe "edit a product process" do
+
+
+  it "edit a product" do
+    product = Product.create(name: "Dog Catcher", cost: "45.00", country_of_origin: "Hawii")
+    visit edit_product_path(product)
+    click_link 'Edit Product'
+    fill_in 'Name', :with => 'Cat Catcher'
+    fill_in 'Cost', :with => '43.00'
+    fill_in 'Country of origin', :with => 'Hawii'
+    click_on 'Update Product'
+    expect(page).to have_content 'Cat Catcher'
+  end
+
+  it "gives error when no name is entered" do
+    product = Product.create(name: "Dog Catcher", cost: "45.00", country_of_origin: "Hawii")
+    visit edit_product_path(product)
+    click_link 'Edit Product'
+    fill_in 'Name', :with => ' '
+    click_on 'Update Product'
+    expect(page).to have_content 'errors'
+  end
+end
